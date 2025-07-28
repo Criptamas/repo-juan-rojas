ebd490d4efaa60eaea8fe141dae13ff0e6358be8
criptamas <criptamas@gmail.com>
2025-07-27 21:41:02 -0400

    Switch between Languages (es/en)

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,10 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  i18n: {
+    locales: ["es", "en"],
+    defaultLocale: "es",
+    prefixDefaultLocale: false
+  }
 });
